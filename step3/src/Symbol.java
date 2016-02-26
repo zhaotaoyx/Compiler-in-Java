1a939ea2e771b06b96e00f605d1c9ba393ad4b74
zhaotaoyx <zhaotao1092@gmail.com>
2016-02-26 16:08:38 -0500

    Added files via upload

---
+++ step3/src/Symbol.java
@@ -0,0 +1,12 @@
+public class Symbol {
+    String name;
+    String type;
+    String value;
+    String functionname;
+    Symbol (String name, String type, String value, String functionname) {
+	this.name = name;
+	this.type = type;
+	this.value = value;
+	this.functionname = functionname;
+    }
+}
---
+++ step3/src/SymbolTable.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class SymbolTable {
+    public static ArrayList<Symbol> symboltable = new ArrayList<Symbol>();
+}
